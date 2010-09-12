291747d7b02ba235e17efb7fdf6c511b7894691a
pierre.tholence@gmail.com <pierre.tholence@gmail.com@dfa30af4-1965-11df-8728-136f2c2ca76e>
2010-09-12 15:45:31 +0000

    Issue 6 added the possibility to use css selector in addition to xpath

--- trunk/defaults/preferences/FireXPathPref.js
+++ trunk/defaults/preferences/FireXPathPref.js
@@ -1,3 +1,4 @@
 pref("extensions.firebug.xpath.showXPathContext", false);
 pref("extensions.firebug.xpath.generateAbsoluteXPath", false);
 pref("extensions.firebug.xpath.showDOM", true);
+pref("extensions.firebug.xpath.evaluationMode", "css");
